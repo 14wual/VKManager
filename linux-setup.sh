25b9a4bb7273c881f340d6db2df368379cc97543
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-05 21:44:20 +0100

    Update linux-setup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,1 +1,12 @@
 
+#--------------------MySQL Installation--------------------
+
+lsb_release -a
+sudo apt update
+
+sudo apt install mysql-server
+sudo systemctl status mysql
+sudo mysql_secure_installation
+sudo mysql
+
+#More Help >> https://www.solvetic.com/tutoriales/article/11746-como-instalar-mysql-en-ubuntu-22-10/
