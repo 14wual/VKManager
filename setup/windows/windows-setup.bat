23c6553d77b42ce0a46091df4dbfd0ba6f189eb5
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-11 11:01:49 +0100

    Update windows-setup.bat

--- setup/windows/windows-setup.bat
+++ setup/windows/windows-setup.bat
@@ -1,23 +1,12 @@
-START
-pip install stdiomask
-PAUSE
-START
-pip install random
-PAUSE
-START
-pip install customtkinter
-PAUSE
-START
-pip install tkinter
-PAUSE
-START
-pip install os
-PAUSE
-START
-pip install datetime
-PAUSE
-START
-pip install mysql.connector
-PAUSE
-START
-pip install Image from PIL
+::    ██╗    ██╗██╗   ██╗ █████╗ ██╗     
+::    ██║    ██║██║   ██║██╔══██╗██║     
+::    ██║ █╗ ██║██║   ██║███████║██║     (code by WUAL)
+::    ██║███╗██║██║   ██║██╔══██║██║     
+::    ╚███╔███╔╝╚██████╔╝██║  ██║███████╗
+::    ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝  
+
+echo off
+
+python conf-script.py
+
+exit
