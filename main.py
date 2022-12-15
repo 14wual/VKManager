ebf165f8909a56e39d12d6713d654981639b2460
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-15 18:00:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pyperclip as clipboard
 import customtkinter
 import tkinter
 from PIL import Image
-import os
 from datetime import datetime
 import mysql.connector
 
@@ -151,8 +150,14 @@ class log(customtkinter.CTk):
 
         self.eactions_colum_frame = customtkinter.CTkFrame(self)
         self.eactions_colum_frame.grid(row=1, column=1, padx=(10, 10), pady=(10, 0), sticky="nsew",columnspan=3,rowspan=3)
-        self.elabel_radio_group = customtkinter.CTkLabel(master=self.eactions_colum_frame, text="Pinned Keys: ",font=customtkinter.CTkFont(weight="bold"))
+        self.eeactions_colum_frame = customtkinter.CTkFrame(self.eactions_colum_frame)
+        self.eeactions_colum_frame.grid(row=3, column=0, padx=(10, 10), pady=(10, 0), sticky="nsew",columnspan=3,rowspan=2)
+        self.eeeactions_colum_frame = customtkinter.CTkFrame(self.eactions_colum_frame)
+        self.eeeactions_colum_frame.grid(row=0, column=0, padx=(10, 10), pady=(10, 0), sticky="nsew",columnspan=3,rowspan=2)
+        self.elabel_radio_group = customtkinter.CTkLabel(master=self.eeeactions_colum_frame, text="📌 Pinned Keys: ",font=customtkinter.CTkFont(weight="bold"))
         self.elabel_radio_group.grid(row=0, column=0, columnspan=1, padx=10, pady=10, sticky="")
+        self.eelabel_radio_group = customtkinter.CTkLabel(master=self.eeactions_colum_frame, text="⌚ Last Search: ",font=customtkinter.CTkFont(weight="bold"))
+        self.eelabel_radio_group.grid(row=0, column=0, columnspan=1, padx=10, pady=10, sticky="")
 
         row_num = 1
         column_num = 0
@@ -170,10 +175,11 @@ class log(customtkinter.CTk):
             )
 
             mycursor = mydb.cursor()
+            lista = [linea.rstrip() for linea in f]
 
-            for line in f:
+            for y in lista:
 
-                sql = "SELECT usser, password FROM vault WHERE site = '%s'" % line
+                sql = "SELECT usser, password FROM vault WHERE site = '%s'" % y
                 mycursor.execute(sql)
                 myresult = mycursor.fetchall()
                 
@@ -182,14 +188,54 @@ class log(customtkinter.CTk):
                         row_num += 1
                         column_num = 0
                     
-                    self.egenerate_frame = customtkinter.CTkFrame(self.eactions_colum_frame)
+                    self.egenerate_frame = customtkinter.CTkFrame(self.eeeactions_colum_frame)
                     self.egenerate_frame.grid(row=row_num, column=column_num, padx=(10, 10), pady=(10, 0), sticky="nsew")
-                    self.elabel_radio_group = customtkinter.CTkLabel(master=self.egenerate_frame, text=f"{line}",font=customtkinter.CTkFont(weight="bold",size=16))
+                    self.elabel_radio_group = customtkinter.CTkLabel(master=self.egenerate_frame, text=f"{y}",font=customtkinter.CTkFont(weight="bold",size=16))
                     self.elabel_radio_group.grid(row=0, column=0, columnspan=1, padx=10, pady=(5,0), sticky="")
                     self.elabel_radio_group = customtkinter.CTkLabel(master=self.egenerate_frame, text=f"{x[0]}",font=customtkinter.CTkFont(size=13))
                     self.elabel_radio_group.grid(row=1, column=0, columnspan=1, padx=10, pady=0, sticky="")
                     self.egenerate_buttom =customtkinter.CTkButton(self.egenerate_frame,text="Copy to Clipboard",command=clipboard.copy(x[1]))
                     self.egenerate_buttom.grid(row=2, column=0, pady=10, padx=5, sticky="n")
+
+                    column_num += 1
+        
+        row_num1 = 1
+        column_num1 = 0
+
+        with open(csv_history_file, 'r') as f:
+
+            usser = self.username_entry.get()
+            passwd = self.password_entry.get()
+
+            mydb = mysql.connector.connect(
+                host="localhost",
+                user=usser,
+                password=passwd,
+                database="mlp"
+            )
+
+            mycursor = mydb.cursor()
+            lista = [linea.rstrip() for linea in f]
+
+            for y in lista:
+
+                sql = "SELECT usser, password FROM vault WHERE site = '%s'" % y
+                mycursor.execute(sql)
+                myresult = mycursor.fetchall()
+                
+                for x in myresult:
+                    if column_num == 5:
+                        row_num1 += 1
+                        column_num1 = 0
+                    
+                    self.eegenerate_frame = customtkinter.CTkFrame(self.eeactions_colum_frame)
+                    self.eegenerate_frame.grid(row=row_num1, column=column_num1, padx=(10, 10), pady=(10, 0), sticky="nsew")
+                    self.eelabel_radio_group = customtkinter.CTkLabel(master=self.eegenerate_frame, text=f"{y}",font=customtkinter.CTkFont(weight="bold",size=16))
+                    self.eelabel_radio_group.grid(row=0, column=0, columnspan=1, padx=10, pady=(5,0), sticky="")
+                    self.eelabel_radio_group = customtkinter.CTkLabel(master=self.eegenerate_frame, text=f"{x[0]}",font=customtkinter.CTkFont(size=13))
+                    self.eelabel_radio_group.grid(row=1, column=0, columnspan=1, padx=10, pady=0, sticky="")
+                    self.eegenerate_buttom =customtkinter.CTkButton(self.eegenerate_frame,text="Copy to Clipboard",command=clipboard.copy(x[1]))
+                    self.eegenerate_buttom.grid(row=2, column=0, pady=10, padx=5, sticky="n")
 
                     column_num += 1
 
@@ -206,13 +252,13 @@ class log(customtkinter.CTk):
 
         self.action_frame = customtkinter.CTkFrame(self)
         self.action_frame.grid(row=2, column=0, padx=(10, 10), pady=(10, 0), sticky="nsew")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Home",command=self.home)
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Home ⭧",command=self.home,fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
         self.generate_pass.grid(row=1, column=0, pady=10, padx=20, sticky="n")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Password Generator",command=self._generatekey)
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Key Generator ⭧",command=self._generatekey)
         self.generate_pass.grid(row=2, column=0, pady=10, padx=20, sticky="n")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Add Key to Vault",command=self._addkey)
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Add Key ⭧",command=self._addkey)
         self.generate_pass.grid(row=3, column=0, pady=10, padx=20, sticky="n")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Modify Site")
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Modify Key ⭧",command=self._modify)
         self.generate_pass.grid(row=4, column=0, pady=10, padx=20, sticky="n")
 
     def sesion_time(self):
@@ -292,6 +338,9 @@ class log(customtkinter.CTk):
 
         mycursor = mydb.cursor()
 
+        with open(csv_history_file, 'w') as f:
+                f.write(f"{mysearch}")
+
         if myfilter == "site":
 
             sql = "SELECT usser, password FROM vault WHERE site = '%s'" % mysearch
@@ -303,13 +352,6 @@ class log(customtkinter.CTk):
             column_num = 0
 
             for x in myresult:
-
-                if empt == True:
-
-                    self.label_radio_sgroup = customtkinter.CTkLabel(master=self.actions_colum_frame, text=f"No results for {mysearch}",font=customtkinter.CTkFont(weight="bold",size=16))
-                    self.label_radio_sgroup.grid(row=1, column=0, columnspan=1, padx=10, pady=(5,0), sticky="")
-
-                else:
 
                     if column_num == 5:
                         row_num += 1
@@ -335,14 +377,7 @@ class log(customtkinter.CTk):
             row_num = 1
             column_num = 0
 
-            for x in myresult:
-
-                if empt == True:
-
-                    self.label_radio_sgroup = customtkinter.CTkLabel(master=self.actions_colum_frame, text=f"No results for {mysearch}",font=customtkinter.CTkFont(weight="bold",size=16))
-                    self.label_radio_sgroup.grid(row=1, column=0, columnspan=1, padx=10, pady=(5,0), sticky="")
-
-                else:
+            for x in myresult:        
 
                     if column_num == 5:
                         row_num += 1
@@ -372,13 +407,13 @@ class log(customtkinter.CTk):
 
         self.action_frame = customtkinter.CTkFrame(self)
         self.action_frame.grid(row=2, column=0, padx=(10, 10), pady=(10, 0), sticky="nsew")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Home",command=self.home)
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Home ⭧",command=self.home)
         self.generate_pass.grid(row=1, column=0, pady=10, padx=20, sticky="n")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Password Generator",command=self._generatekey)
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Key Generator ⭧",command=self._generatekey)
         self.generate_pass.grid(row=2, column=0, pady=10, padx=20, sticky="n")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Add Key to Vault",command=self._addkey)
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Add Key ⭧",command=self._addkey)
         self.generate_pass.grid(row=3, column=0, pady=10, padx=20, sticky="n")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Modify Site")
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Modify Key ⭧",command=self._modify)
         self.generate_pass.grid(row=4, column=0, pady=10, padx=20, sticky="n")
 
     #--------------------3.2 - Key Generate--------------------
@@ -483,13 +518,13 @@ class log(customtkinter.CTk):
         
         self.action_frame = customtkinter.CTkFrame(self)
         self.action_frame.grid(row=2, column=0, padx=(10, 10), pady=(10, 0), sticky="nsew")
-        self.home =customtkinter.CTkButton(self.action_frame,text="Home",command=self.home)
+        self.home =customtkinter.CTkButton(self.action_frame,text="Home ⭧",command=self.home)
         self.home.grid(row=1, column=0, pady=10, padx=20, sticky="n")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Password Generator",command=self._generatekey)
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Key Generator ⭧",command=self._generatekey)
         self.generate_pass.grid(row=2, column=0, pady=10, padx=20, sticky="n")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Add Key to Vault",command=self._addkey)
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Add Key ⭧",command=self._addkey)
         self.generate_pass.grid(row=3, column=0, pady=10, padx=20, sticky="n")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Modify Site")
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Modify Key ⭧",command=self._modify)
         self.generate_pass.grid(row=4, column=0, pady=10, padx=20, sticky="n")
     
     def slider_event(self,value):
@@ -565,13 +600,13 @@ class log(customtkinter.CTk):
         
         self.action_frame = customtkinter.CTkFrame(self)
         self.action_frame.grid(row=2, column=0, padx=(10, 10), pady=(10, 0), sticky="nsew")
-        self.home =customtkinter.CTkButton(self.action_frame,text="Home",command=self.home)
+        self.home =customtkinter.CTkButton(self.action_frame,text="Home ⭧",command=self.home)
         self.home.grid(row=1, column=0, pady=10, padx=20, sticky="n")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Password Generator",command=self._generatekey)
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Key Generator ⭧",command=self._generatekey)
         self.generate_pass.grid(row=2, column=0, pady=10, padx=20, sticky="n")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Add Key to Vault",command=self._addkey)
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Add Key ⭧",command=self._addkey)
         self.generate_pass.grid(row=3, column=0, pady=10, padx=20, sticky="n")
-        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Modify Site")
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Modify Key ⭧",command=self._modify)
         self.generate_pass.grid(row=4, column=0, pady=10, padx=20, sticky="n")
     
     def event__addkey(self):
@@ -603,6 +638,103 @@ class log(customtkinter.CTk):
         if onfo == "on":
             with open(conf_pinned_file, 'a') as f:
                 f.write(f"\n{site}")
+
+    def event__modify(self):
+
+        usser = self.username_entry.get()
+        passwd = self.password_entry.get()
+
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=usser,
+            password=passwd,
+            database="mlp"
+        )
+            
+        passwds = self.entry_pwsd.get()
+        site = self.entry_site.get()
+        ussers = self.entry_user.get()
+
+        mycursor = mydb.cursor()
+        sql = "UPDATE vault SET usser = '%s' WHERE site = '%s'" % ussers, site
+        sql1 = "UPDATE vault SET password = '%s' WHERE site = '%s'" % passwds, site
+        mycursor.execute(sql, sql1)
+
+        mydb.commit()
+    
+    #--------------------3.3 - Add Key--------------------
+    def _modify(self):
+        self.time_sesion = datetime.now()
+
+        self.main_frame.grid_forget()
+        self.title(f"VKManager | Modify Key")
+
+        self.resizable(False, False)
+        self.search_frame = customtkinter.CTkFrame(self)
+        self.geometry(f"{1100}x{595}")
+
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2, 3), weight=0)
+
+        self.entry_search = customtkinter.CTkEntry(self, placeholder_text="Search your passwd")
+        self.entry_search.grid(row=0, column=0, columnspan=2, padx=(10, 10), pady=(10, 0), sticky="nsew")
+        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"),text="Search",command=self.search_fr)
+        self.main_button_1.grid(row=0, column=3, padx=(10, 10), pady=(10, 0), sticky="nsew")
+
+        self.radiobutton_frame = customtkinter.CTkFrame(self)
+        self.radiobutton_frame.grid(row=1, column=0, padx=(10, 10), pady=(10, 0), sticky="nsew")
+        self.radio_var = tkinter.IntVar(value=0)
+        self.label_radio_group = customtkinter.CTkLabel(master=self.radiobutton_frame, text="Search filters:",font=customtkinter.CTkFont(weight="bold"))
+        self.label_radio_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
+        self.filter_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=0,text="Search by site")
+        self.filter_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
+        self.filter_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=1,text="Search by user")
+        self.filter_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
+        self.filter_button_3 = customtkinter.CTkRadioButton(master=self.radiobutton_frame)
+        self.filter_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
+        self.filter_button_3.configure(state="disabled", text="Search by pass")
+
+        self.actions_colum_frame = customtkinter.CTkFrame(self)
+        self.actions_colum_frame.grid(row=1, column=1, padx=(10, 10), pady=(10, 0), sticky="nsew",columnspan=3,rowspan=3)
+        self.main_frame.grid_forget()
+
+        self.label_radio_group = customtkinter.CTkLabel(master=self.actions_colum_frame, text="Site: ",font=customtkinter.CTkFont(weight="bold"))
+        self.label_radio_group.grid(row=1, column=0, columnspan=1, padx=10, pady=10, sticky="")
+        self.entry_site = customtkinter.CTkEntry(self.actions_colum_frame, placeholder_text="Site")
+        self.entry_site.grid(row=1, column=1, padx=(10, 10), pady=(10, 0), sticky="nsew")
+        self.label_radio_group = customtkinter.CTkLabel(master=self.actions_colum_frame, text="User: ",font=customtkinter.CTkFont(weight="bold"))
+        self.label_radio_group.grid(row=2, column=0, columnspan=1, padx=10, pady=10, sticky="")
+        self.entry_user = customtkinter.CTkEntry(self.actions_colum_frame, placeholder_text="User")
+        self.entry_user.grid(row=2, column=1, columnspan=2, padx=(10, 10), pady=(10, 0), sticky="nsew")
+        self.label_radio_group = customtkinter.CTkLabel(master=self.actions_colum_frame, text="Password: ",font=customtkinter.CTkFont(weight="bold"))
+        self.label_radio_group.grid(row=3, column=0, columnspan=1, padx=10, pady=10, sticky="")
+        self.entry_pwsd = customtkinter.CTkEntry(self.actions_colum_frame, placeholder_text="Password",show="*")
+        self.entry_pwsd.grid(row=3, column=1, columnspan=2, padx=(10, 10), pady=(10, 0), sticky="nsew")
+        self.aae_pass =customtkinter.CTkButton(self.actions_colum_frame,text="Add Key to Vault",command=self.event__modify)
+        self.aae_pass.grid(row=5, column=0, pady=(20,10), padx=20, sticky="n", columnspan=3)
+
+        self.sesion_frame = customtkinter.CTkFrame(self)
+        self.sesion_frame.grid(row=3, column=0, padx=(10, 10), pady=(10, 0), sticky="nsew")
+        self.label_radio_group = customtkinter.CTkLabel(master=self.sesion_frame, text="Current Sesion:",font=customtkinter.CTkFont(weight="bold"))
+        self.label_radio_group.grid(row=0, column=0, columnspan=1, padx=10, pady=10, sticky="")
+        self.label_radio_group = customtkinter.CTkLabel(master=self.sesion_frame, text=f"User: {self.username_entry.get()}",justify=tkinter.LEFT)
+        self.label_radio_group.grid(row=1, column=0, columnspan=1, padx=10, pady=2.5, sticky="")
+        self.label_radio_group = customtkinter.CTkLabel(master=self.sesion_frame,justify=tkinter.LEFT,textvariable="")
+        self.label_radio_group.grid(row=2, column=0, columnspan=1, padx=10, pady=2.5, sticky="")
+        self.label_radio_group = customtkinter.CTkLabel(master=self.sesion_frame, text="Time left >>  ",justify=tkinter.LEFT)
+        self.label_radio_group.grid(row=3, column=0, columnspan=1, padx=10, pady=2.5, sticky="")
+        
+        self.action_frame = customtkinter.CTkFrame(self)
+        self.action_frame.grid(row=2, column=0, padx=(10, 10), pady=(10, 0), sticky="nsew")
+        self.home =customtkinter.CTkButton(self.action_frame,text="Home ⭧",command=self.home)
+        self.home.grid(row=1, column=0, pady=10, padx=20, sticky="n")
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Key Generator ⭧",command=self._generatekey)
+        self.generate_pass.grid(row=2, column=0, pady=10, padx=20, sticky="n")
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Add Key ⭧",command=self._addkey)
+        self.generate_pass.grid(row=3, column=0, pady=10, padx=20, sticky="n")
+        self.generate_pass =customtkinter.CTkButton(self.action_frame,text="Modify Key ⭧",command=self._modify)
+        self.generate_pass.grid(row=4, column=0, pady=10, padx=20, sticky="n")
 
 def vault(usser,passwd):
     try:
