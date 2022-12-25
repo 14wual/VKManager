e6260ceeda1c669c231721b6f954599aacbbc8b8
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-25 23:31:47 +0100

    Update banner.py

--- app/__gui__/banner.py
+++ app/__gui__/banner.py
@@ -1,30 +1,20 @@
+# ██╗    ██╗██╗   ██╗ █████╗ ██╗     
+# ██║    ██║██║   ██║██╔══██╗██║     
+# ██║ █╗ ██║██║   ██║███████║██║     (code by wual)
+# ██║███╗██║██║   ██║██╔══██║██║     
+# ╚███╔███╔╝╚██████╔╝██║  ██║███████╗
+#  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
+
+# BV0.9.5
+# See proyect >> https://github.com/14wual/VKManager
+# Follow me >> https://twitter.com/codewual
+
 #--------------------Extern Imports--------------------
-import webbrowser
-import random
-import pyperclip as clipboard
 import customtkinter
 import tkinter
 import tkinter as tk
-from PIL import Image
 from datetime import datetime
 import mysql.connector
-
-#--------------------VAR & CONST--------------------
-with open('conf/appearance.conf','r') as appearance_file:
-    conf_appearance_mode = appearance_file.readline()
-    customtkinter.set_appearance_mode(conf_appearance_mode)
-
-csv_login_file = 'logs\log.csv'
-csv_history_file = 'logs\search_history.csv'
-conf_pinned_file= 'conf\pinned.conf'
-
-now = datetime.now()
-REFRESH_INTERVAL = 200
-
-search_filter_var_value = 0
-
-pages = ['home','addkey','generatekey','modifykey','search']
-page = pages[0]
 
 #--------------------APP--------------------
 def main(self):
