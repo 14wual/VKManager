d575b70457822c4fd5a3415681747ec84aa58d00
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-26 22:25:13 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='VKManager',
+    version='0.9.5',
+    description='Vault Keys Manager It is a password manager with interface programmed in python with the help of CustomTkinter (CTK) and MySQL.',
+    url='https://github.com/14wual/VKManager',
+    author='Carlos Padilla Labella',
+    author_email='cpadlab@gmail.com',
+    license='GPL',
+    install_requires=[
+        'customtkinter',
+        'mysql.connector',
+        'pillow',
+        'pyperclip',
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/14wual/VKManager/issues',
+        'Source': 'https://github.com/14wual/VKManager/blob/main/main.py',
+    },
+)
