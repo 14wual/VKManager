733826f8ec62ad771168a337653dc585066f3687
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-27 16:37:50 +0100

    Create script_update.py

---
+++ app/__other__/script_update.py
@@ -0,0 +1,55 @@
+# ██╗    ██╗██╗   ██╗ █████╗ ██╗     
+# ██║    ██║██║   ██║██╔══██╗██║     
+# ██║ █╗ ██║██║   ██║███████║██║     (code by wual)
+# ██║███╗██║██║   ██║██╔══██║██║     
+# ╚███╔███╔╝╚██████╔╝██║  ██║███████╗
+#  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
+
+# BV1.0.1
+# See proyect >> https://github.com/14wual/VKManager
+# Follow me >> https://twitter.com/codewual
+
+#--------------------External Imports--------------------
+import requests
+import customtkinter
+import inspect
+import os
+
+#--------------------External Imports--------------------
+import __init__
+
+#--------------------APP--------------------
+
+def main(self):
+    response = requests.get("https://github.com/14wual/VKManager/raw/main/about/version")
+    file_contents = response.text
+
+    string = "VKM V1.0.0\n"
+
+    if file_contents == string:
+        update_dialog(self,file_contents)    
+    else:
+        pass
+
+def update_dialog(self,update):
+
+    self.update_windows = customtkinter.CTkToplevel(self)
+    self.update_windows.title(f"Update: {update}")
+    self.update_windows.resizable(0, 0)
+    self.update_windows.wm_attributes("-topmost", 1)
+
+    self.update_label = customtkinter.CTkLabel(self.update_windows, text="New update available!!",font=customtkinter.CTkFont(weight="bold",size=16))
+    self.update_label.grid(row=0, column=0, pady=20, padx=20, sticky="n",columnspan=2)
+
+    self.update_button =customtkinter.CTkButton(self.update_windows,text="Do later",command=lambda:update_dialog_destroy(self))
+    self.update_button.grid(row=1, column=0, pady=10, padx=20, sticky="n")
+
+    self.update_button_1 =customtkinter.CTkButton(self.update_windows,text="Update!",command=lambda:update_dialog_event(self))
+    self.update_button_1.grid(row=1, column=1, pady=20, padx=10, sticky="n")
+
+def update_dialog_destroy(self):
+    self.update_windows.destroy()
+
+def update_dialog_event(self):
+
+    __init__.App.update_dialog_event(self)
