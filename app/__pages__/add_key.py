f0842edfbd73e3b8a4a14dd57d354b8fbf22d9f4
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-28 13:50:06 +0100

    Update add_key.py

--- app/__pages__/add_key.py
+++ app/__pages__/add_key.py
@@ -5,7 +5,7 @@
 # ╚███╔███╔╝╚██████╔╝██║  ██║███████╗
 #  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
 
-# BV0.9.7
+# BV1.2.3
 # See proyect >> https://github.com/14wual/VKManager
 # Follow me >> https://twitter.com/codewual
 
