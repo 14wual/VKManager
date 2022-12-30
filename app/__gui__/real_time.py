aa100602ec0577d2340e1b2d111d77087598dbc3
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-31 00:47:00 +0100

    Update real_time.py

--- app/__gui__/real_time.py
+++ app/__gui__/real_time.py
@@ -32,14 +32,16 @@ def main(self,real_time_search):
     self.real_time_item_var.set(value=real_time_search)
 
     self.real_time_item_button = customtkinter.CTkButton(self.search_panel_frame,textvariable=self.real_time_item_var,
-                command=lambda search=self.real_time_item_var:search_history_item_button_event(self,search),
+                command=lambda:search_history_item_button_event(self),
                 width=420,height=32,border_width=0,anchor="w",fg_color="transparent")
     self.real_time_item_button.grid(row=10,column=1,padx=10,pady=0)
 
     self.image_label = customtkinter.CTkLabel(self.search_panel_frame,text="",image=self.search_image)
     self.image_label.grid(row=10,column=0,padx=10,pady=0)
 
-def search_history_item_button_event(self,mysearch):
+def search_history_item_button_event(self):
+
+    mysearch = self.real_time_item_var.get()
 
     now = datetime.now()
     formatted_time = now.strftime("%d/%m/%Y %H:%M:%S")
