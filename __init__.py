5c4759b99ff9e5d63555822389d637213c1473c2
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-31 14:10:03 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@
 # ╚███╔███╔╝╚██████╔╝██║  ██║███████╗
 #  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
 
-# BV0.9.5
+# BV1.3.9
 # See proyect >> https://github.com/14wual/VKManager
 # Follow me >> https://twitter.com/codewual
 
@@ -13,6 +13,8 @@
 import customtkinter
 import os
 import atexit
+import tkinter.messagebox
+from datetime import datetime
 
 #--------------------Intern Imports--------------------
 from app.__pages__ import login
@@ -22,17 +24,27 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
+        now  = datetime.now()
+
         os.mkdir("conf/temp/")
         file_temp = open("conf/temp/credentials.tmp","w")
         
         login.login(self)
 
-        atexit.register(self.on_closing)
+        atexit.register(lambda time=now:self.on_closing(time))
 
-    def on_closing(self):
+    def on_closing(self,time):
 
         try:os.remove("conf/temp/credentials.tmp")
         finally:os.rmdir("conf/temp/")
+
+        current_time_sesion = datetime.now() - time
+
+        print(current_time_sesion)
+
+        message = f"Time in session {current_time_sesion}\n"
+        tkinter.messagebox.askyesno("Close application", message)
+
         
 if __name__ == '__main__':
     app = App()
