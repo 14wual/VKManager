a51a120ccf70e3bead2dbfdf0db1ed40c2ca18bd
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-31 12:26:11 +0100

    Update script_update.py

--- app/__other__/script_update.py
+++ app/__other__/script_update.py
@@ -5,7 +5,7 @@
 # ╚███╔███╔╝╚██████╔╝██║  ██║███████╗
 #  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
 
-# BV1.2.2
+# BV1.3.8
 # See proyect >> https://github.com/14wual/VKManager
 # Follow me >> https://twitter.com/codewual
 
