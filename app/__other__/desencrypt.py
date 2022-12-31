dfabfe8e482e9c0be3f6e7db2981780356fcbc27
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-31 12:25:18 +0100

    Update desencrypt.py

--- app/__other__/desencrypt.py
+++ app/__other__/desencrypt.py
@@ -1,3 +1,15 @@
+# ██╗    ██╗██╗   ██╗ █████╗ ██╗     
+# ██║    ██║██║   ██║██╔══██╗██║     
+# ██║ █╗ ██║██║   ██║███████║██║     (code by wual)
+# ██║███╗██║██║   ██║██╔══██║██║     
+# ╚███╔███╔╝╚██████╔╝██║  ██║███████╗
+#  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
+
+# BV1.3.8
+# See proyect >> https://github.com/14wual/VKManager
+# Follow me >> https://twitter.com/codewual
+
+
 from cryptography.fernet import Fernet
 
 def decrypt(encrypted_password: bytes, key: bytes) -> str:
