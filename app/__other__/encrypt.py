37d05753bdcc68dbf4cd9ac980eec9caed4e7e1a
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-31 12:25:39 +0100

    Update encrypt.py

--- app/__other__/encrypt.py
+++ app/__other__/encrypt.py
@@ -1,3 +1,14 @@
+# ██╗    ██╗██╗   ██╗ █████╗ ██╗     
+# ██║    ██║██║   ██║██╔══██╗██║     
+# ██║ █╗ ██║██║   ██║███████║██║     (code by wual)
+# ██║███╗██║██║   ██║██╔══██║██║     
+# ╚███╔███╔╝╚██████╔╝██║  ██║███████╗
+#  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
+
+# BV1.3.8
+# See proyect >> https://github.com/14wual/VKManager
+# Follow me >> https://twitter.com/codewual
+
 from cryptography.fernet import Fernet
 
 def encrypt(password: str) -> bytes:
