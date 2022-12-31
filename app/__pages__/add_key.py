0685b82d9ebf8357dc1d79157a90b8d784cd5d04
Wual1014 <105047274+14wual@users.noreply.github.com>
2022-12-31 12:00:52 +0100

    Update add_key.py

--- app/__pages__/add_key.py
+++ app/__pages__/add_key.py
@@ -5,115 +5,181 @@
 # ╚███╔███╔╝╚██████╔╝██║  ██║███████╗
 #  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
 
-# BV1.0.0
+# BV0.9.7
 # See proyect >> https://github.com/14wual/VKManager
 # Follow me >> https://twitter.com/codewual
 
 #--------------------External Imports--------------------
+import pyperclip as clipboard
 import customtkinter
+from datetime import datetime
+import mysql.connector
 import tkinter
 from PIL import Image
-import mysql.connector
 
 #--------------------Internal Imports--------------------
-from app.__other__ import encrypt
+from app.__gui__ import filter_buttons
+from app.__other__ import desencrypt
 
 #--------------------VAR & CONS--------------------
-conf_pinned_file= 'conf\pinned.conf'
+csv_history_file = 'logs\search_history.csv'
 
 pages = ['home','addkey','generatekey','modifykey','search']
 page = pages[0]
 
 #--------------------APP--------------------
-def main(self):
+def main(self,mysearch):
     global pages; global page
 
     try:self.content_frame_page_home.grid_forget()
     except:
         try:self.content_frame_page_modify_key.grid_forget()
         except:
-            try:self.content_frame_page_search.grid_forget()
+            try:self.content_frame_page_add_key.grid_forget()
             except:self.content_frame_page_generate_key.grid_forget()
     finally:
-        page = pages[1]            
+        page = pages[-1]
 
-    add_key(self)
+    search(self,mysearch)
 
-def add_key(self):
+def is_list_empty(list):
+    return list == []
 
-    self.content_frame_page_add_key = customtkinter.CTkFrame(self)
-    self.content_frame_page_add_key.grid(row=1, column=1, padx=(3, 10), pady=(10, 10), sticky="nsew",columnspan=3,rowspan=3)
+def search(self,mysearch):
 
-    self.add_key_label = customtkinter.CTkLabel(self.content_frame_page_add_key, text="Add Key",font=customtkinter.CTkFont(size=20, weight="bold"))
-    self.add_key_label.pack(padx=20, pady=(75,10))
+        with open("conf/temp/credentials.tmp","r") as credentials_file:
+            credentials_list = [linea.rstrip() for linea in credentials_file]
+        
+        usser = credentials_list[0]
+        passwd = credentials_list[1]
 
-    self.entry_site_var = tkinter.StringVar()
-    self.entry_site = customtkinter.CTkEntry(self.content_frame_page_add_key, placeholder_text="Enter your site")
-    self.entry_site.pack(padx=20, pady=10)
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=usser,
+            password=passwd,
+            database="mlp"
+        )
 
-    self.entry_user_var = tkinter.StringVar()
-    self.entry_user = customtkinter.CTkEntry(self.content_frame_page_add_key, placeholder_text="Enter your username")
-    self.entry_user.pack(padx=20, pady=10)
+        mycursor = mydb.cursor()
 
-    self.key_image = customtkinter.CTkImage(light_image=Image.open("images\key-dark.png"),
-                              dark_image=Image.open("images\key.png"),
-                              size=(13, 13))
+        for y in mysearch:
 
-    self.entry_password_var = tkinter.StringVar()
-    self.entry_password = customtkinter.CTkEntry(self.content_frame_page_add_key, placeholder_text="Enter your password",show="*")
-    self.entry_password.pack(padx=20, pady=10)
+            self.title(f"VKManager | Search: {mysearch}")
 
-    self.add_to_pinned_var = tkinter.StringVar(value="off")
-    self.add_to_pinned_checkbox = customtkinter.CTkCheckBox(self.content_frame_page_add_key, text="Add to Pinned Keys",
-                                        variable=self.add_to_pinned_var, onvalue="on", offvalue="off")
-    self.add_to_pinned_checkbox.pack(padx=20, pady=10)
+            self.content_frame_page_search = customtkinter.CTkFrame(self)
+            self.content_frame_page_search.grid(row=1, column=1, padx=(3, 10), pady=(10, 10), sticky="nsew",columnspan=3,rowspan=3)
 
-    self.add_key_button =  customtkinter.CTkButton(self.content_frame_page_add_key, text="Add Key to Vault", command=lambda:add_key_to_vault_event(self))
-    self.add_key_button.pack(padx=20, pady=10)
+            myfilter = filter_search(self)
 
-def add_key_to_vault_event(self):
-    
-    site = self.entry_site.get()
-    user = self.entry_user.get()
-    passw = self.entry_password.get()
+            now = datetime.now()
+            formatted_time = now.strftime("%d/%m/%Y %H:%M:%S")
+            formatted_date = now.strftime("%d/%m/%Y")
 
-    with open("conf/temp/credentials.tmp","r") as credentials_file:
-        credentials_list = [linea.rstrip() for linea in credentials_file]
-            
-    usser = credentials_list[0]
-    passwd = credentials_list[1]
+            with open(csv_history_file, 'a') as f:
+                    f.write(f"\n{mysearch},{formatted_time},{formatted_date}")
 
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user=usser,
-        password=passwd,
-        database="mlp"
-    )
-    
-    mycursor = mydb.cursor()
+            if myfilter == "site":
 
-    encrypts = encrypt.encrypt(passw)
-    sql_pass_list = []
+                sql = "SELECT usser, password, encrkey FROM vault WHERE site = '%s'" % mysearch
+                mycursor.execute(sql)
+                myresult = mycursor.fetchall()
 
-    for u in encrypts:
-        sql_pass = u.decode()
-        sql_pass_list.append(sql_pass)
+                row_num = 1
+                column_num = 0
+                break_for = 0
 
-    sql = "INSERT INTO vault (site, usser, password, encrkey) VALUES (%s, %s, %s, %s)"
-    val = (f"{site}",f"{user}",f"{sql_pass_list[0]}",f"{sql_pass_list[1]}")
-    mycursor.execute(sql, val)
+                if is_list_empty(myresult) == True:
 
-    mydb.commit()
+                    self.x_image = customtkinter.CTkImage(light_image=Image.open("images\cross.png"),
+                                  dark_image=Image.open("images\cross.png"),
+                                  size=(230, 230))
 
-    add_pinned = self.add_to_pinned_var.get()
+                    self.no_content_image = customtkinter.CTkLabel(self.content_frame_page_search,image=self.x_image,text='')
+                    self.no_content_image.place(relx=0.5,rely=0.4,anchor=tkinter.CENTER)
 
-    if add_pinned == "off":
-        pass
-    elif add_pinned == "on":
-        with open(conf_pinned_file, 'a') as f:
-            f.write(f"\n{site}")
+                    self.no_content_var = tkinter.IntVar(value=f"There are no results for {mysearch}:{myfilter}")
+                    self.no_content_label = customtkinter.CTkLabel(self.content_frame_page_search,textvariable=self.no_content_var,font=customtkinter.CTkFont(weight="bold",size=16))
+                    self.no_content_label.place(relx=0.5,rely=0.8,anchor=tkinter.CENTER)
+                else:
+                    for x in myresult:
 
-    self.added_key_label = customtkinter.CTkLabel(self.content_frame_page_add_key, text="Added key",font=customtkinter.CTkFont(weight="bold"),text_color="green")
-    self.added_key_label.pack(padx=20, pady=10)
+                        if break_for == 15:break
 
-    self.add_to_pinned_var.set("off")
+                        if column_num == 5:
+                            row_num += 1
+                            column_num = 0
+                        
+                        passwd = desencrypt.decrypt(x[1],x[2])
+
+                        self.generate_result_frame = customtkinter.CTkFrame(self.content_frame_page_search)
+                        self.generate_result_frame.grid(row=row_num, column=column_num, padx=(10, 10), pady=(10, 10), sticky="nsew")
+
+                        self.generate_key_site_label_1 = customtkinter.CTkLabel(master=self.generate_result_frame, text=f"{mysearch}",font=customtkinter.CTkFont(weight="bold",size=16))
+                        self.generate_key_site_label_1.grid(row=0, column=0, columnspan=1, padx=10, pady=(5,0), sticky="")
+
+                        self.generate_key_user_label_1 = customtkinter.CTkLabel(master=self.generate_result_frame, text=f"{x[0]}",font=customtkinter.CTkFont(size=13))
+                        self.generate_key_user_label_1.grid(row=1, column=0, columnspan=1, padx=10, pady=0, sticky="")
+
+                        self.generate_copy_button_1 =customtkinter.CTkButton(self.generate_result_frame,text="Copy to Clipboard",
+                            command=lambda password=passwd: clipboard.copy(password))
+                        self.generate_copy_button_1.grid(row=2, column=0, pady=10, padx=5, sticky="n")
+
+                        column_num += 1
+
+            elif myfilter == "usser":
+                        
+                sql = "SELECT site, password, encrkey FROM vault WHERE usser = '%s'" % mysearch
+                mycursor.execute(sql)
+                myresult = mycursor.fetchall()
+                
+                row_num = 1
+                column_num = 0
+
+                if is_list_empty(myresult) == True:
+
+                    self.x_image = customtkinter.CTkImage(light_image=Image.open("images\cross.png"),
+                                  dark_image=Image.open("images\cross.png"),
+                                  size=(230, 230))
+
+                    self.no_content_image = customtkinter.CTkLabel(self.content_frame_page_search,image=self.x_image,text='')
+                    self.no_content_image.place(relx=0.5,rely=0.4,anchor=tkinter.CENTER)
+
+                    self.no_content_var = tkinter.IntVar(value=f"There are no results for {mysearch}:{myfilter}")
+                    self.no_content_label = customtkinter.CTkLabel(self.content_frame_page_search,textvariable=self.no_content_var,font=customtkinter.CTkFont(weight="bold",size=16))
+                    self.no_content_label.place(relx=0.5,rely=0.8,anchor=tkinter.CENTER)
+                else:
+
+                    for x in myresult:        
+
+                            if column_num == 5:
+                                row_num += 1
+                                column_num = 0
+
+                            passwd = desencrypt.decrypt(x[1],x[2])
+
+                            self.generate_result_frame = customtkinter.CTkFrame(self.content_frame_page_search)
+                            self.generate_result_frame.grid(row=row_num, column=column_num, padx=(10, 10), pady=(10, 10), sticky="nsew")
+
+                            self.generate_key_site_label_1 = customtkinter.CTkLabel(master=self.generate_result_frame, text=f"{x[0]}",font=customtkinter.CTkFont(weight="bold",size=16))
+                            self.generate_key_site_label_1.grid(row=0, column=0, columnspan=1, padx=10, pady=(5,0), sticky="")
+
+                            self.generate_key_user_label_1 = customtkinter.CTkLabel(master=self.generate_result_frame, text=f"{mysearch}",font=customtkinter.CTkFont(size=13))
+                            self.generate_key_user_label_1.grid(row=1, column=0, columnspan=1, padx=10, pady=0, sticky="")
+
+                            self.generate_copy_button_1 =customtkinter.CTkButton(self.generate_result_frame,text="Copy to Clipboard",
+                                command=lambda password=passwd: clipboard.copy(password))
+                            self.generate_copy_button_1.grid(row=2, column=0, pady=10, padx=5, sticky="n")
+                            column_num += 1
+
+def filter_search(self):
+
+        value = filter_buttons.search_filter_var_value
+        
+        Valfilter = ""
+
+        if value == 0:
+            Valfilter = "site"
+        elif value == 1:
+            Valfilter = "usser"
+
+        return Valfilter  
