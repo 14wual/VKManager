41eab9f9ec97e8defb7a003a3dc033d0ac2aa84d
Wual1014 <105047274+14wual@users.noreply.github.com>
2023-01-09 22:31:40 +0100

    Update generate_key.py

--- app/__pages__/generate_key.py
+++ app/__pages__/generate_key.py
@@ -5,7 +5,7 @@
 # ╚███╔███╔╝╚██████╔╝██║  ██║███████╗
 #  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
 
-# BV1.3.8
+# BV1.5.8
 # See proyect >> https://github.com/14wual/VKManager
 # Follow me >> https://twitter.com/codewual
 
@@ -14,13 +14,8 @@ import random
 import pyperclip as clipboard
 import customtkinter
 
-#--------------------VAR & CONS--------------------
-pages = ['home','addkey','generatekey','modifykey','search']
-page = pages[0]
-
 #--------------------APP--------------------
 def main(self):
-        global pages; global page
 
         try:self.content_frame_page_home.grid_forget()
         except:
@@ -29,11 +24,10 @@ def main(self):
                 try:self.content_frame_page_add_key.grid_forget()
                 except:self.content_frame_page_search.grid_forget()
         finally:
-            page = pages[2]
-        
-        generate_key(self)
+            self.page = self.pages[2];generate_key(self)
 
 def generate_key(self):
+    """Graphical interface for customizing passwords, and explanation of their security levels."""
 
     self.content_frame_page_generate_key = customtkinter.CTkFrame(self)
     self.content_frame_page_generate_key.grid(row=1, column=1, padx=(3, 10), pady=(10, 10), sticky="nsew",columnspan=3,rowspan=3)
@@ -47,31 +41,15 @@ def generate_key(self):
     self.levels_pass_tabview.set("Level 3")  
 
     self.level_1_tabview = customtkinter.CTkLabel(self.levels_pass_tabview.tab("Level 1"),text='A password of X characters based on \n\
-        uppercaseand lowercase letters will be generated\
-        \n\nCharacters:\
-        \nABCDEFGHIJKLMNÑOPQRSTUVWXYZ\
-        \nabcdefghijklmnñopqrstuvwxyz\
-        \n\nExamples:\
-        \nJXHDIgGFpbpoQbv\
-        \neWgQVÑÑMmVrB',font=customtkinter.CTkFont(weight="bold",size=16))
+        uppercaseand lowercase letters will be generated\n\nCharacters:\nABCDEFGHIJKLMNÑOPQRSTUVWXYZ\nabcdefghijklmnñopqrstuvwxyz\n\nExamples:\nJXHDIgGFpbpoQbv\neWgQVÑÑMmVrB',font=customtkinter.CTkFont(weight="bold",size=16))
     self.level_1_tabview.pack(padx=20, pady=20)
 
     self.level_2_tabview = customtkinter.CTkLabel(self.levels_pass_tabview.tab("Level 2"),text='A password of X characters based on \n\
-        uppercaseand lowercase letters & numbers will \nbe generated\
-        \n\n+= Characters:\
-        \n1234567890\
-        \n\nExamples:\
-        \nX2XwOUx5SKyG\
-        \nWvS570gHDtGv',font=customtkinter.CTkFont(weight="bold",size=16))
+        uppercaseand lowercase letters & numbers will \nbe generated\n\n+= Characters:\n1234567890\n\nExamples:\nX2XwOUx5SKyG\nWvS570gHDtGv',font=customtkinter.CTkFont(weight="bold",size=16))
     self.level_2_tabview.pack(padx=20, pady=20)
 
     self.level_3_tabview = customtkinter.CTkLabel(self.levels_pass_tabview.tab("Level 3"),text='A password of X characters based on \n\
-        uppercaseand lowercase letters, numbers & special\ncharacter will be generated\
-        \n\n+= Characters:\
-        \nºª!|·#$%&¬/()=?¿¡.:-_,;*{,}ç^[+]\
-        \n\nExamples:\
-        \nc6ÑqbSºHer]J\
-        \nyew}I]a¿rN^m',font=customtkinter.CTkFont(weight="bold",size=16))
+        uppercaseand lowercase letters, numbers & special\ncharacter will be generated\n\n+= Characters:\nºª!|·#$%&¬/()=?¿¡.:-_,;*{,}ç^[+]\n\nExamples:\nc6ÑqbSºHer]J\nyew}I]a¿rN^m',font=customtkinter.CTkFont(weight="bold",size=16))
     self.level_3_tabview.pack(padx=20, pady=20)
 
     self.select_level_sec_var = customtkinter.StringVar(value="Level 3") 
@@ -91,6 +69,8 @@ def delete_window(self):
     self.window.destroy()
     
 def pass_generator(self):
+    """It collects the personalization data of the key and generates one from this data.
+    Generate a frame where you show the password"""
 
     let = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
     car = "ºª!|·#$%&¬/()=?¿¡.:-_,;*{,}ç^[+]"
@@ -99,18 +79,11 @@ def pass_generator(self):
     long = round(self.select_long_sec_button.get())
     level = self.select_level_sec_var.get()
 
-    lists = ""
-    password = ""
+    lists = "";password = ""
         
-    if level == "Level 1":
-        lists += let
-    elif level == "Level 2":
-        lists += let
-        lists += num
-    elif level == "Level 3":
-        lists += let
-        lists += num
-        lists += car
+    if level == "Level 1":lists += let
+    elif level == "Level 2":lists += let;lists += num
+    elif level == "Level 3":lists += let;lists += num;lists += car
 
     for x in range(long):
         a = random.choice(str(lists))
